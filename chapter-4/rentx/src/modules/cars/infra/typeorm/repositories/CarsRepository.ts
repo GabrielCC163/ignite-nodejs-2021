c832dfa9fdbb3cb4c7715843f508a30e5d1af87b
Gabriel Brum <gabriel.brum@anestech.com.br>
2021-09-10 20:44:20 -0300

    feat: car specifications

--- chapter-4/rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ chapter-4/rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -13,6 +13,7 @@ class CarsRepository implements ICarsRepository {
   }
 
   async create({
+    id,
     brand,
     category_id,
     daily_rate,
@@ -20,8 +21,10 @@ class CarsRepository implements ICarsRepository {
     fine_amount,
     license_plate,
     name,
+    specifications,
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
+      id,
       brand,
       category_id,
       daily_rate,
@@ -29,6 +32,7 @@ class CarsRepository implements ICarsRepository {
       fine_amount,
       license_plate,
       name,
+      specifications,
     });
 
     await this.repository.save(car);
@@ -39,6 +43,37 @@ class CarsRepository implements ICarsRepository {
   async findByLicensePlate(license_plate: string): Promise<Car> {
     const car = await this.repository.findOne({ license_plate });
 
+    return car;
+  }
+
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder()
+      .where('available = :available', { available: true });
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere('name = :name', { name });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
     return car;
   }
 }
--- chapter-4/rentx/src/modules/cars/repositories/ISpecificationsRepository.ts
+++ chapter-4/rentx/src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../infra/typeorm/entities/Specification';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 
 interface ICreateSpecificationDTO {
   name: string;
@@ -12,6 +12,8 @@ interface ISpecificationsRepository {
   }: ICreateSpecificationDTO): Promise<Specification>;
 
   findByName(name: string): Promise<Specification>;
+
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
